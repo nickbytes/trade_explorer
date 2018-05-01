51d8467f5f560a9f96c603997ed2c0b5d593ee6a
Nick Beattie <nick@nickbytes.com>
2018-04-30 21:28:39 -0400

    chill

--- server/credentials.js
+++ server/credentials.js
@@ -1,3 +1,7 @@
+//
+// chill, these are sandbox api keys
+//
+
 module.exports.API_KEY = "69a0bdae2eb72662330884fcaf8296c4";
 module.exports.API_SECRET =
   "e2kwd+js84QQBFnTnkzwX9ETf/5qzhB8BVHQ4+ou8aYqJqsxT0xFOu2NeLrH4h+UldalqL2T9DUHwmvEePyvEg==";
